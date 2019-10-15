f69396297fbc2dcab14134000215d75628e962c7
jshad0 <jshad0@users.noreply.github.com>
2019-10-15 13:45:49 -0400

    todo app update

---
+++ Todo app/client.js
@@ -0,0 +1,165 @@
+
+/**
+ * Client side script for Shado Todo app
+ */
+
+    var toDoList = [];
+
+    async function getData()    {
+        const request = await fetch("/readDB");
+        const json = await request.json();
+        const data = json.results;
+
+        toDoList = data;
+    }
+
+    function formateTime(time)  {
+
+        // Convert time to seconds from milliseconds
+        time = time / 1000;
+
+        // time in seocnds
+        var days = Math.floor(time / 86400);
+        var hours = Math.floor((time - (days * 86400)) / 3600);
+        var min = Math.floor((time -  (days * 86400) - (hours * 3600)) / 60 );
+        var seconds = Math.floor( time -  (days * 86400) - (hours * 3600) - (min * 60) );
+
+        days = days < 10 ? ('0' + days) : days;
+        hours = hours < 10 ? ('0' + hours) : hours;
+        min = min < 10 ? ('0' + min) : min;
+        seconds = seconds < 10 ? ('0' + seconds) : seconds;
+
+        return `${days}D ${hours}:${min}:${seconds}`;
+
+    }
+
+    function displayData(data)  {
+
+        const BODY = document.getElementById("toDo_display");
+
+        var str = `<table class="formated_table">
+            <tr class="first_row">
+                <th>Color</th>
+                <th>Task</th>
+                <th>Deadline</th>
+                <th>Time left</th>
+                <th>Options</th>
+            </tr>`;
+
+        // Sort data by the most urgent to least urgent
+        data.sort(function(a, b)    {
+            var t1 = new Date(a.deadline).getTime() - new Date().getTime();
+            var t2 = new Date(b.deadline).getTime() - new Date().getTime();
+            return t1 - t2;
+        }); 
+
+        for (let i = 0; i < data.length; i++)   {
+
+            // Format the remaining time and date
+            var timeLeft =  new Date(data[i].deadline).getTime() - new Date().getTime();
+            var dateString = formateTime(timeLeft);
+
+            var style = "";
+             
+            // Add colors to show emergency
+            if ( Math.abs(timeLeft) < 6 * 60 * 60 * 1000) {
+                style = "background-color: rgba(255, 0, 0, .4); color: white; font-weight: bold;";
+            } else if ( Math.abs(timeLeft) < 12 * 60 * 60 * 1000)    {
+                style = "background-color: yellow;"
+            }
+
+            str += "<tr>";
+            
+            str += `<td><div class="round" style="background-color: ${data[i].color}; color: ${data[i].color};">.</div></td>
+                    <td>${data[i].todo}</td>
+                    <td style="text-align: center;">${new Date(data[i].deadline).toLocaleString()}</td>
+                    <td style="${style} text-align: center;" id="${data[i]._id}_td">${dateString}</td>
+                    <td style="text-align: center;">
+                        <img src="delete-trash-pngrepo-com.png" OnClick="deleteToDo('${data[i]._id}')" height="25"/>
+                    </td>`
+
+            str += "</tr>";
+
+        }
+
+        str += "</table>";
+
+        BODY.innerHTML = str;
+    }
+
+    async function addData()  {
+
+        var task = document.getElementById("task").value;
+        var tempDeadline = document.getElementById("deadline").value;
+
+        tempDeadline = tempDeadline.split("-");
+        
+        // Convert dead line from YYYY-MM-DD to 
+        var deadline_hours = document.getElementById("deadline_hour").value;
+        deadline_hours = deadline_hours.split(":");
+
+        var deadline = new Date(Number(tempDeadline[0]), Number(tempDeadline[1] - 1), Number(tempDeadline[2]), Number(deadline_hours[0]), Number(deadline_hours[1]) );
+
+        var color = document.getElementById("color").value;
+
+        const data = { todo: task, deadline: deadline, color: color };
+		
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+
+        const response = await fetch('/addTodo', options);	
+        const json = await response.json();
+
+        getData();
+
+    }
+
+    async function deleteToDo(id)   {
+
+		const data = {id: id};
+		
+		const options = {
+			method: 'POST',
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify(data)
+		};
+			
+		const response = await fetch('/deleteTodo', options);
+        
+        for (let i = 0; i < 10; i++)    {
+            getData();
+        }		
+
+    }
+
+    async function updateToDo(id)   {
+
+		const data = {id: id};
+		
+		const options = {
+			method: 'POST',
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify(data)
+		};
+			
+		const response = await fetch('/updateTodo', options);
+        
+        for (let i = 0; i < 10; i++)    {
+            getData();
+        }	
+    }
+
+    getData();
+
+    setInterval(function()  {
+        displayData(toDoList);
+    }, 1000);
